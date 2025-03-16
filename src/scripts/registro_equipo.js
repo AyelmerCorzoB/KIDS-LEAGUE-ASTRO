903ce91314fab850c57f3148c1b9c6945d7c6fe6
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-03-16 16:36:46 -0500

    feat: :sparkles: Mejorar el diseño y la funcionalidad de varios componentes, incluyendo ajustes en estilos y la adición de un nuevo script para el registro de equipos

---
+++ src/scripts/registro_equipo.js
@@ -0,0 +1,36 @@
+// Selecciona los botones por su ID
+const botonSiguiente = document.getElementById("mostrarFormEntrenador");
+const botonRegistrar = document.getElementById("registrarEquipo");
+
+// Selecciona los formularios por su ID
+const formEquipo = document.getElementById("formEquipo");
+const formEntrenador = document.getElementById("formEntrenador");
+
+botonSiguiente.addEventListener("click", () => {
+  mostrarFormEntrenador();
+});
+
+botonRegistrar.addEventListener("click", () => {
+  RegistrarJugador();
+});
+
+function mostrarFormEntrenador() {
+  if (formEquipo && formEntrenador) {
+    formEquipo.classList.add("hidden");
+    formEntrenador.classList.remove("hidden");
+  }
+}
+
+function RegistrarJugador() {
+  const nombreEntrenador = document.getElementById("nombreEntrenador").value;
+  const telefonoEntrenador =
+    document.getElementById("TelefonoEntrenador").value;
+  const emailEntrenador = document.getElementById("telefonoEntrenador").value;
+
+  if (nombreEntrenador && telefonoEntrenador && emailEntrenador) {
+    alert("¡Registro completado correctamente!");
+    window.location.href = "/registro";
+  } else {
+    alert("Por favor, complete todos los campos requeridos.");
+  }
+}
